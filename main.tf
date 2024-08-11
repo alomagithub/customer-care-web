33ee80bcfb0984a86fba8589e3f4e8106402a9c2
Idris <ec2-user@ip-172-31-86-149.ec2.internal>
2024-08-11 12:43:13 +0000

    name changed to Aloma

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name   = "Aloma"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
